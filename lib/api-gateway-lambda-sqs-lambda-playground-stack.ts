5af1e0258a6c4179bb7567f8bf36ab269764ed81
Nakajima <kituneudon97@gmail.com>
2023-07-08 16:13:07 +0900

    2 create resources (#5)
    * #2 とりあえずAPI Gateway→Lambdaの構成を構築
    
    * vscodeのスペルチェッカーでapigwを除外するようにした
    
    * #2 データが最後のLambdaまで行くことを確認した

--- lib/api-gateway-lambda-sqs-lambda-playground-stack.ts
+++ lib/api-gateway-lambda-sqs-lambda-playground-stack.ts
@@ -1,16 +1,46 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export class ApiGatewayLambdaSqsLambdaPlaygroundStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create the SQS queue
+    const queue = new Queue(this, 'Queue');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ApiGatewayLambdaSqsLambdaPlaygroundQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create the Lambda function for pushing data to SQS
+    const pushDataToQueueFunction = new Function(this, 'PushDataToQueueFunction', {
+      runtime: Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: Code.fromAsset('lambda/pushDataToQueueFunction'),
+      environment: {
+        QUEUE_URL: queue.queueUrl
+      },
+      timeout: cdk.Duration.seconds(10),
+    });
+
+    // Create the Lambda function for processing data from SQS
+    const processDataFromQueueFunction = new Function(this, 'ProcessDataFromQueueFunction', {
+      runtime: Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: Code.fromAsset('lambda/processDataFromQueueFunction'),
+      timeout: cdk.Duration.seconds(10),
+      environment: {
+        QUEUE_URL: queue.queueUrl
+      },
+    });
+    processDataFromQueueFunction.addEventSource(new SqsEventSource(queue));
+
+    // Create the API Gateway
+    new LambdaRestApi(this, 'Endpoint', {
+      handler: pushDataToQueueFunction
+    });
+
+    pushDataToQueueFunction.addToRolePolicy(new PolicyStatement({ actions: ['sqs:SendMessage'], resources: [queue.queueArn] }))
   }
 }
